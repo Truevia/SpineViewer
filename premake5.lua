4da14cf7289cdaca66ec1425c40cd8cc565328f4
via <via@host>
2025-11-15 20:30:46 +0400

    feat: macOS GUI app

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,8 @@ workspace "SpineViewerProject"
     configurations { "Debug", "Release" }
     location "projects"
 
+local macos_deployment_target = "11.0"
+
 spine_versions = { 
     "2.1.25", "2.1.08",
     "4.2", 
@@ -13,8 +15,9 @@ for k,version in ipairs(spine_versions) do
     project_name = "SpineViewer" .. version
     print(project_name)
     project (project_name)
-        kind "ConsoleApp"
-        -- kind "WindowedApp"
+        -- kind "ConsoleApp"
+        kind "WindowedApp"
+        local bundle_identifier = "com.trve." .. project_name
 
         language "C++"
         cppdialect "C++11"
@@ -78,7 +81,10 @@ for k,version in ipairs(spine_versions) do
             libdirs { "vendor/glfw/lib" }
             links { "Cocoa.framework", "OpenGL.framework", "IOKit.framework", "CoreVideo.framework", "glfw3" }
             xcodebuildsettings {
-                ALWAYS_SEARCH_USER_PATHS = "YES"
+                ALWAYS_SEARCH_USER_PATHS = "YES",
+                GENERATE_INFOPLIST_FILE = "YES",
+                PRODUCT_BUNDLE_IDENTIFIER = bundle_identifier,
+                MACOSX_DEPLOYMENT_TARGET = macos_deployment_target,
             }
         defines { "GLFW_INCLUDE_NONE", "IMGUI_IMPL_OPENGL_LOADER_CUSTOM" }
 
