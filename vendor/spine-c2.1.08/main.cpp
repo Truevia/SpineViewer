9ff88b3520bbdf854fb4f04a03cac956659130e4
via <via@host>
2025-11-16 13:10:56 +0400

    feat: popup error ui for windows

--- vendor/spine-c2.1.08/main.cpp
+++ vendor/spine-c2.1.08/main.cpp
@@ -130,6 +130,10 @@ static void drop_callback(GLFWwindow * /*window*/, int count, const char **paths
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+
+#ifdef _WIN32
+    MessageBoxA(NULL, description, "GLFW Error", MB_OK | MB_ICONERROR);
+#endif
 }
 
 // Main code
